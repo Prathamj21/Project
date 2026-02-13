fbc74e63e7a9da80d54de90fe6ab71aee3c366fd
Prathamj21 <prathamj2194@gmail.com>
2026-02-13 13:11:02 +0530

    Version update

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Live Project - Version 1 🚀');
+  res.send('DevOps Live Project - Version 2 🚀');
 });
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
+
